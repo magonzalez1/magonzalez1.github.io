e7163279953e8b7595bc515dd940256dc965d9aa
magonzalez1 <magonzalez1@users.noreply.github.com>
2015-04-02 09:06:40 -0400

    Update application.js

--- guess_my_number/application.js
+++ guess_my_number/application.js
@@ -1,15 +1,18 @@
 $(document).ready(function(){
   //find a way for the program to choose a random number between 1 and 100, save this as a variable
   var CurrentIndex = 100
+  var click_count = 0
   randomIndex = Math.floor(Math.random()*100);
   //when the player clicks on the 'guess' button
   $('button').on('click', function(){
     //save their guess as a variable
     var userGuess = $('input').val();
+    click_count = click_count+1
+    console.log(click_count)
     console.log(userGuess)
     //and compare this guess to the random number that the computer picked
     //if the user guessed the correct number...
-    if (userGuess === randomIndex){
+    if (userGuess == randomIndex){
       alert("You win")
     }
       //what happens if the guess is correct?
