75d34ec700792c9f4653104f185bb9edf82bc99d
magonzalez1 <magonzalez1@users.noreply.github.com>
2015-04-16 08:17:15 -0400

    Update application.js

--- shopping/application.js
+++ shopping/application.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
   $('#submit').on('click', function(){
     var entry = $('input').val();
-    $('ul').append("<li>"+entry+"</li>");
+    $('ul').append("<li>"+entry+"</li>"); //the item will be appended into the ul as a li element.
+  })
+  $('#clear').on('click', function(){
+    var entry = $('input').val();
+    $("ul").empty(); //the items will be removed from the ul.
   })
 });
+
